0f5341fc90da5fbd0b66d76ffff16abc468ea694
Volodymyr025 <Fudo2790@gmail.com>
2024-01-06 14:42:26 +0200

    Create validation form with formik and yup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Login } from "./component/pages/Login";
 import Registration from "./component/pages/Registration";
 
 function App() {
   return <>
   <Registration/>
+  <Login/>
   </>;
 }
 
--- src/component/util/error.tsx
+++ src/component/util/error.tsx
@@ -1,24 +1,7 @@
 export const ERROR_MESSAGE = {
-    emptyField: (
-      <p style={{ color: "black", fontSize: 18 }}>The field must not be empty</p>
-    ),
-    emailField: (
-      <p style={{ color: "black", fontSize: 18 }}>The email field is incorrect</p>
-    ),
-    phoneField: (
-      <p style={{ color: "black", fontSize: 18 }}>
-        The phone number field is incorrect
-      </p>
-    ),
-    passwordField: (
-      <p style={{ color: "black", fontSize: 18 }}>
-        The Password field must have 6-16 characters and one symbol (!@#$%^&*())
-      </p>
-    ),
-    confirmField: (
-      <p style={{ color: "black", fontSize: 18 }}>
-        The Passwords must must match
-      </p>
-    ),
-  };
-  
+  emptyField: "The field must not be empty",
+  emailField: "The email field is incorrect",
+  phoneField: "The phone number field is incorrect",
+  passwordField:"The Password field must have 6-16 characters and one symbol (!@#$%^&*())",
+  confirmField: "The Passwords must match",
+};
