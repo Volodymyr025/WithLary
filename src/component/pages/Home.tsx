688432e89f6a1d623976c0ca7426449494f1eeb7
Volodymyr025 <Fudo2790@gmail.com>
2024-01-08 12:32:42 +0200

    Create home page

---
+++ src/component/pages/Home.tsx
@@ -0,0 +1,37 @@
+import { Box } from "@mui/material";
+import style from './Home.module.css'
+
+
+const Home = () => {
+  return (
+    <Box
+      sx={{
+        bgcolor: "#caaacd",
+        width: "100%",
+        height: "100%",
+        display: "grid",
+        gridTemplate: "1fr/1fr 1fr",
+        textAlign: "center",
+      }}
+    >
+      <Box>
+        <h1 className="logoLary">With Lary</h1>
+        <p className="underLogo">Help Ma&Pa</p>
+      </Box>
+      <Box >
+        <ul className={style.mainRight}>
+        <li>Child development from 0-36</li>
+        <li>Everything for feeding</li>
+        <li>Care, hygiene</li>
+        <li>Toys and creativity</li>
+        <li>
+          <a href={"sleep"}>Peaceful sleep</a>
+        </li>
+        <li>Children's room and security</li>
+        </ul>
+      </Box>
+    </Box>
+  );
+};
+
+export default Home;
